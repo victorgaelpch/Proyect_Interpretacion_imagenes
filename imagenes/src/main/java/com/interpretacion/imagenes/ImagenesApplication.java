842dd63077e0963a5e5ca77d54483d7514530261
victor <victorgaelpch@gmail.com>
2026-05-30 16:24:32 -0600

    Estructura inicial del proyecto

---
+++ imagenes/src/main/java/com/interpretacion/imagenes/ImagenesApplication.java
@@ -0,0 +1,13 @@
+package com.interpretacion.imagenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagenesApplication.class, args);
+	}
+
+}
---
+++ imagenes/src/test/java/com/interpretacion/imagenes/ImagenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interpretacion.imagenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
