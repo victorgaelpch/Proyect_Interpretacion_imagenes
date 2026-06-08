85790f2996c0deb771c81f46445129b8ac84c41d
ImMateo <matthew.fco.14@gmail.com>
2026-06-08 01:12:38 -0600

    Se implementó un dto extra (punto x,y) y modificó para que retorne todos los datos recibidos desde FastApi

--- imagenes/src/main/java/com/interpretacion/imagenes/Dto/RespuestaImagenEditada.java
+++ imagenes/src/main/java/com/interpretacion/imagenes/Dto/RespuestaImagenEditada.java
@@ -1,19 +1,69 @@
 package com.interpretacion.imagenes.Dto;
 
+import java.util.List;
+
 public class RespuestaImagenEditada {
     private String nombreImagenOriginal;
     private String nombreImagenEditada;
+    private String nombreImagenEscaneada;
+    private List<PuntoDTO> coordenadas;
+    private Integer anchoResultado;
+    private Integer altoResultado;
+    private Double umbral;
+
+    public List<PuntoDTO> getCoordenadas() {
+        return coordenadas;
+    }
+
+    public void setCoordenadas(List<PuntoDTO> coordenadas) {
+        this.coordenadas = coordenadas;
+    }
+
     public String getNombreImagenOriginal() {
         return nombreImagenOriginal;
     }
+
     public void setNombreImagenOriginal(String nombreImagenOriginal) {
         this.nombreImagenOriginal = nombreImagenOriginal;
     }
+
     public String getNombreImagenEditada() {
         return nombreImagenEditada;
     }
+
     public void setNombreImagenEditada(String nombreImagenEditada) {
         this.nombreImagenEditada = nombreImagenEditada;
     }
-    
+
+    public Integer getAnchoResultado() {
+        return anchoResultado;
+    }
+
+    public void setAnchoResultado(Integer anchoResultado) {
+        this.anchoResultado = anchoResultado;
+    }
+
+    public Integer getAltoResultado() {
+        return altoResultado;
+    }
+
+    public void setAltoResultado(Integer altoResultado) {
+        this.altoResultado = altoResultado;
+    }
+
+    public Double getUmbral() {
+        return umbral;
+    }
+
+    public void setUmbral(Double umbral) {
+        this.umbral = umbral;
+    }
+
+    public String getNombreImagenEscaneada() {
+        return nombreImagenEscaneada;
+    }
+
+    public void setNombreImagenEscaneada(String nombreImagenEscaneada) {
+        this.nombreImagenEscaneada = nombreImagenEscaneada;
+    }
 }
--- imagenes/src/main/java/com/interpretacion/imagenes/services/ImageSaveService.java
+++ imagenes/src/main/java/com/interpretacion/imagenes/services/ImageSaveService.java
@@ -18,57 +18,64 @@ import com.interpretacion.imagenes.exceptions.*;
 @Service
 public class ImageSaveService {
     private final RestClient restClient;
-        public ImageSaveService(){
-            restClient=RestClient.builder()
-            .baseUrl("http://localhost:8000/")
-            .build();
-        }
-                private final String RUTA_BASE="C:\\imagenesAnalizadas\\";
 
-        public RespuestaImagenEditada saveImage(MultipartFile imagen) throws IOException{
-
-            tipoImagen(imagen);
-            String extencion=imagen.getOriginalFilename().substring(imagen.getOriginalFilename().lastIndexOf("."));
-            String nombreSeguro= UUID.randomUUID().toString()+"_imagenOriginal"+extencion;
-            if(rutaNoExiste()) {
-                crearRuta();
-            }
-            File file=new File(RUTA_BASE+nombreSeguro);
-            try {
-                imagen.transferTo(file);
-            } catch (Exception e) {
-            throw new ImagenException("Error al guardar la imagen en disco: " + e.getMessage());
-            }
-            RespuestaImagenEditada respuesta = restClient.post()
-            .uri("/editarImagen")
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(Map.of("nombre", nombreSeguro))
-            .retrieve()
-            .body(RespuestaImagenEditada.class);
-            RespuestaImagenEditada res = new RespuestaImagenEditada();
-            res.setNombreImagenOriginal(respuesta.getNombreImagenOriginal());
-            res.setNombreImagenEditada(respuesta.getNombreImagenEditada());
-
-            return res;
-            
-            
+    public ImageSaveService() {
+        restClient = RestClient.builder()
+                .baseUrl("http://localhost:8000/")
+                .build();
     }
-        private void tipoImagen(MultipartFile imagen) throws IOException{
-            List<String> tipoPermitidos= List.of("image/png","image/jpeg","image/jpg","image/webp");
-            Tika tika = new Tika();
-            String contentType = tika.detect(imagen.getInputStream());
-            if(!tipoPermitidos.contains(contentType)){
+
+    private final String RUTA_BASE = "C:\\imagenesAnalizadas\\";
+
+    public RespuestaImagenEditada saveImage(MultipartFile imagen) throws IOException {
+
+        tipoImagen(imagen);
+        String extencion = imagen.getOriginalFilename().substring(imagen.getOriginalFilename().lastIndexOf("."));
+        String nombreSeguro = UUID.randomUUID().toString() + "_imagenOriginal" + extencion;
+        if (rutaNoExiste()) {
+            crearRuta();
+        }
+        File file = new File(RUTA_BASE + nombreSeguro);
+        try {
+            imagen.transferTo(file);
+        } catch (Exception e) {
+            throw new ImagenException("Error al guardar la imagen en disco: " + e.getMessage());
+        }
+        RespuestaImagenEditada respuesta = restClient.post()
+                .uri("/editarImagen")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("nombre", nombreSeguro))
+                .retrieve()
+                .body(RespuestaImagenEditada.class);
+        /*
+         * RespuestaImagenEditada res = new RespuestaImagenEditada();
+         * res.setNombreImagenOriginal(respuesta.getNombreImagenOriginal());
+         * res.setNombreImagenEditada(respuesta.getNombreImagenEditada());
+         * res.setCoordenadas(respuesta.getCoordenadas());
+         */
+
+        return respuesta;
+
+    }
+
+    private void tipoImagen(MultipartFile imagen) throws IOException {
+        List<String> tipoPermitidos = List.of("image/png", "image/jpeg", "image/jpg", "image/webp");
+        Tika tika = new Tika();
+        String contentType = tika.detect(imagen.getInputStream());
+        if (!tipoPermitidos.contains(contentType)) {
             throw new ImagenContentTypeException("El tipo de imagen no es permitido");
-            }  
         }
-        private boolean rutaNoExiste() {
-            File ruta=new File(RUTA_BASE);
-            return !ruta.exists();
-        }
-        private void crearRuta() {
-            File ruta=new File(RUTA_BASE);
-            ruta.mkdirs();
-            System.out.println("Ruta creada: " + RUTA_BASE);
-        }
-    
+    }
+
+    private boolean rutaNoExiste() {
+        File ruta = new File(RUTA_BASE);
+        return !ruta.exists();
+    }
+
+    private void crearRuta() {
+        File ruta = new File(RUTA_BASE);
+        ruta.mkdirs();
+        System.out.println("Ruta creada: " + RUTA_BASE);
+    }
+
 }
